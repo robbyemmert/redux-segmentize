0db698518f690f431a19171d8e6986bb9f86f79e
Robby Emmert <robby.emmert@cqlcorp.com>
2017-03-09 09:14:37 -0500

    updating dist

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ var devConfig = {
 }
 
 var buildConfig = {
-    entry: './src/redux-segmentize.jsx',
+    entry: './src/redux-segmentize.js',
     output: {
         library: libraryName,
         libraryTarget: 'umd',
